07da309aa5e1917542b1c71c3dfe67f6b72555df
SarthakM1 <sarthak.mahajan54@gmail.com>
2020-09-22 21:35:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const form = dcoument.getElementById('form');
+const username = dcoument.getElementById('username');
+const email = document.getElementById('email');
+const password = dcoument.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+}
+
+if(usernameValue === '') {
+    setErrorFor(username, 'Username cannot be blank');
+} else {
+    setSuccessFor(username);
+}
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+}
